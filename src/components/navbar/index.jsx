5c2209bea095b55a8c862e97c9f88b82a839185b
MuhammadDarab <darabmonib123@gmail.com>
2024-04-03 14:13:47 +0500

    animations added.

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (<div className="flex backdrop-blur-md fixed p-14 bg-transparent z-10 w-full">
         <div>M.</div>
-        <div>M.</div>
+        <div>Darab</div>
     </div>);
 }
  
---
+++ src/utils/mappings.js
@@ -0,0 +1,156 @@
+export const techStacksList = [
+  {
+    title: "React",
+    label: "react",
+  },
+  {
+    title: "Node",
+    label: "nodejs",
+  },
+  {
+    title: "Sequelize",
+    label: "sequelize",
+  },
+  {
+    title: "Socket IO",
+    label: "socketio",
+  },
+  {
+    title: "Git",
+    label: "git",
+  },
+  {
+    title: "Ubuntu",
+    label: "debian",
+  },
+  {
+    title: "Trello",
+    label: "trello",
+    forceURL:
+      "https://cdn.jsdelivr.net/npm/devicon@2.14.0/icons/trello/trello-plain.svg",
+  },
+  {
+    title: "Three Js",
+    label: "threejs",
+  },
+  {
+    title: "MongoDb",
+    forceURL: "./MaterialDesignMongo.png",
+  },
+  {
+    title: "Redux",
+    label: "redux",
+  },
+  {
+    title: "VSCode",
+    label: "vscode",
+  },
+  {
+    title: "Postgres",
+    label: "...",
+    forceURL: "./MaterialDesignPostgres.png",
+  },
+  {
+    title: "Electron",
+    label: "electron",
+  },
+  {
+    title: "Express",
+    label: "express",
+  },
+  {
+    title: "Tailwind",
+    label: "tailwindcss",
+    forceURL:
+      "https://cdn.jsdelivr.net/npm/devicon@2.14.0/icons/tailwindcss/tailwindcss-plain.svg",
+  },
+  {
+    title: "Material UI",
+    label: "materialui",
+  },
+  {
+    title: "Heroku",
+    label: "heroku",
+  },
+  {
+    title: "React Native",
+    label: "react",
+    customClass: "hue-rotate-[265deg]",
+  },
+  {
+    title: "Javascript",
+    label: "javascript",
+  },
+  {
+    title: "Figma",
+    label: "figma",
+  },
+  {
+    title: "Vite",
+    label: "vite",
+    forceURL: "./vite.svg",
+  },
+  {
+    title: "Docker",
+    label: "",
+    forceURL: "./lucide_container.png",
+    author: "https://github.com/lucide-icons/lucide",
+  },
+  {
+    title: "Typescript",
+    label: "typescript",
+  },
+  {
+    title: "Terminal",
+    label: "terminal",
+    forceURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Windows_Terminal_logo.svg/1280px-Windows_Terminal_logo.svg.png",
+  },
+  {
+    title: "Redis",
+    label: "redis",
+    lock: true,
+  },
+  {
+    title: "GraphQl",
+    label: "graphql",
+    forceURL:
+      "https://cdn.jsdelivr.net/npm/devicon@2.14.0/icons/graphql/graphql-plain.svg",
+    lock: true,
+  },
+  {
+    title: "Scss",
+    label: "scss",
+    forceURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/1280px-Sass_Logo_Color.svg.png",
+    lock: false,
+  },
+];
+
+export const projectsList = [
+  {
+    title: "Cantt College CMS.",
+    description:
+      "Text that is set temporarily in place of actual copy is called “placeholder text” or “dummy text.” It often takes the form of “lorem ipsum”—a sequence of words from a work of classical literature that is deliberately intended to not be read (in modern times). This is often referred to as “Latin” and—erroneously—“Greek,” or “Greeked.”",
+  },
+  {
+    title: "Laser Box UI",
+    description:
+      "The purpose of any placeholder text, including lorem ipsum, is to indicate that the text is not there to be read, which is why it’s used in design mockups, where the viewer should be focussing on the overall design, and in type specimens, where the reader should be focussing on the characters’s personality and design, and overall typographic color.",
+  },
+  {
+    title: "Minni Chat",
+    description:
+      "A Websocket based React native application, where you can send and retrieve your messages.",
+  },
+  {
+    title: "Salah Reminder App.",
+    description:
+      "Fonts and icons makes it easy to integrate expressive type and icons",
+  },
+  {
+    title: "Mimic Nike UI",
+    description:
+      "Google Fonts makes it easy to bring personality and performance to your websites and products. Our robust catalog of open-source fonts and icons makes it easy to integrate expressive type and icons",
+  },
+];
